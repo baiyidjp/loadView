872f30fbecbc6cf252e99695d06dca7c55fa71e9
baiyidjp <271524878@qq.com>
2017-01-10 15:08:46 +0800

    Initial Commit

---
+++ loadView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loadView
+//
+//  Created by Keep丶Dream on 17/1/10.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ loadView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  loadView
+//
+//  Created by Keep丶Dream on 17/1/10.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
